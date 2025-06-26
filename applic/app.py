492c259e866b7644d377dd4232d837863a84f965
alijmalijm <alirezajafarimoghaddam36@Gmail.com>
2025-06-26 13:08:36 +0330

    Update app.py

--- applic/app.py
+++ applic/app.py
@@ -1,10 +1,8 @@
+import os
 from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "سلام عشق من"
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000,debug=True)
